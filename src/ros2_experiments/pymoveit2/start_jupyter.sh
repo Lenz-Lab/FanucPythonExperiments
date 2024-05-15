da058015a578db036bcf14650a5a2e297cb748f7
CasuallyAlive <Jordy.larrearodriguez@gmail.com>
2024-05-15 11:59:22 -0600

    Added script for setuping up jupyter server.

---
+++ src/ros2_experiments/pymoveit2/start_jupyter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# source ros2 humble
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+# source pymoveit2 ws 
+source $PYMOVEIT2/install/local_setup.bash
+
+# source fanuc packages in current ws
+source install/local_setup.bash
+
+/home/$USER/.local/bin/jupyter-notebook
