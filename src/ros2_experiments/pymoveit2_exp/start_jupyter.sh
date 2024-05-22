e054e2da77c03e76b1844e5e2e96ec5d8c3ac4a3
CasuallyAlive <Jordy.larrearodriguez@gmail.com>
2024-05-22 15:31:33 -0600

    initial commit.

--- src/ros2_experiments/pymoveit2_exp/start_jupyter.sh
+++ src/ros2_experiments/pymoveit2_exp/start_jupyter.sh
@@ -9,4 +9,4 @@ source $PYMOVEIT/install/local_setup.bash
 # source fanuc packages in current ws
 source install/local_setup.bash
 
-/home/$USER/.local/bin/jupyter-notebook
+/home/$USER/.local/bin/jupyter-notebook --detach
